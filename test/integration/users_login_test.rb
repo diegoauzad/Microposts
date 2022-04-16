65132f3bbc67b11ec3a0956700264ccdb00b5fb3
Diego Auza Derpic <diego.auza.d@gmail.com>
2022-04-16 02:35:01 -0400

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -55,9 +55,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "remember checked in log in should persist in cookies" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "remember not checked in log in should not persist in cookies" do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:user_id].blank?
+    assert cookies[:remember_token].blank?
   end
 end
